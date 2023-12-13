d5535d379d2be84efb23eaf603a24e6a1dcef1ee
Orbital-G <agnica0404@gmail.com>
2023-12-13 22:42:48 +0900

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jojo/Team_Daisangen/src
+# Build directory: /home/jojo/Team_Daisangen/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
