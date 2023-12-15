f0602214b9b2654eb759ad50027acbf2a7422480
Orbital-G <agnica0404@gmail.com>
2023-12-16 01:01:57 +0900

    add as a package

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/jojo/Team_Daisangen/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jojo/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/jojo/catkin_ws/devel;/opt/ros/noetic' != '' else []
