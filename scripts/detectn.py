40c9ffc7163f57a5fa49737b63039c2c0b3560e2
Orbital-G <agnica0404@gmail.com>
2023-12-18 03:39:26 +0900

    change camera port number

--- scripts/detectn.py
+++ scripts/detectn.py
@@ -257,7 +257,7 @@ def run(
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'cyasen.pt', help='model path or triton URL')
-    parser.add_argument('--source', type=int, default=8, help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--source', type=int, default=1, help='file/dir/URL/glob/screen/0(webcam)')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
